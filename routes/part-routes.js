eb52ce41354cba214e5569ffbdbc64fc5a23fc23
braedonb98 <brbe3574@colorado.edu>
2022-04-08 22:34:39 -0600

    adding part routes

---
+++ routes/part-routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const partController = require("../controllers/part-controller");
+const checkAuth = require("../middleware/check-auth");
+const router = express.Router();
+
+//router.use(checkAuth); // every route after this requires an token
+
+router.post("/part", partController.createPart); //starts new count
+
+router.patch("/part", partController.editPart); //sets item count and returns next part in count if the part has already been counted update that part count and return next item
+
+router.delete("/part", partController.deletePart); //removes entire count item
+
+router.get("/part", partController.getPart); //gets specific count by id
+
+module.exports = router;
